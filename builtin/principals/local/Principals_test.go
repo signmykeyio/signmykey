5d3fcc9eeb130f3a7a70f38b023aedd89dc0eb2d
Pablo RUTH <contact@pablo-ruth.fr>
2018-08-09 12:51:51 +0200

    update local principals backend to use viper interface

--- builtin/principals/local/Principals_test.go
+++ builtin/principals/local/Principals_test.go
@@ -1,53 +1,67 @@
 package local
 
 import (
+	"bytes"
 	"sort"
 	"testing"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPrincipals(t *testing.T) {
 
 	cases := []struct {
-		userMap map[string]string
+		userMap []byte
 		user    string
 		expErr  bool
 		expList []string
 	}{
 		{
-			map[string]string{"user1": "princ2,princ1"}, "user1",
-			false, []string{"princ1", "princ2"},
+			[]byte(`user1: princ2,princ1`),
+			"user1", false, []string{"princ1", "princ2"},
 		},
 		{
-			map[string]string{"user": "princ1,princ2"}, "user",
-			false, []string{"princ1", "princ2"},
+			[]byte(`user: princ1,princ2`),
+			"user", false, []string{"princ1", "princ2"},
 		},
 		{
-			map[string]string{"user2": "princ1,princ2"}, "user1",
-			true, []string{},
+			[]byte(`user2: princ1,princ2`),
+			"user1", true, []string{},
 		},
 		{
-			map[string]string{"user1": "princ1", "user2": "princ3,princ4"}, "user2",
-			false, []string{"princ3", "princ4"},
+			[]byte(`
+user1: princ1
+user2: princ3,princ4
+`),
+			"user2", false, []string{"princ3", "princ4"},
 		},
 		{
-			map[string]string{"user1": "princ1, princ2,princ3 ", "user2": "princ3,princ4"}, "user1",
-			false, []string{"princ1", "princ2", "princ3"},
+			[]byte(`
+user1: princ1, princ2,princ3
+user2: princ3,princ4
+`),
+			"user1", false, []string{"princ1", "princ2", "princ3"},
 		},
 		{
-			map[string]string{"user1": "princ1, princ2,princ3 , ,princ4", "user2": "princ3,princ4"}, "user1",
-			false, []string{"princ1", "princ2", "princ3", "princ4"},
-		},
-		{
-			map[string]string{"user1": "  , ,    , ", "user2": "princ3,princ4"}, "user1",
-			true, []string{},
+			[]byte(`
+user1: princ1, princ2,princ3 , ,princ4
+user2: princ3,princ4
+`),
+			"user1", false, []string{"princ1", "princ2", "princ3", "princ4"},
 		},
 	}
 
 	for _, c := range cases {
+		testConfig := viper.New()
+		testConfig.SetConfigType("yaml")
+		err := testConfig.ReadConfig(bytes.NewBuffer(c.userMap))
+		if err != nil {
+			t.Error(err)
+		}
+
 		local := &Principals{}
-		local.Init(c.userMap)
+		local.Init(testConfig)
 
 		principals, err := local.Get(c.user)
 		if c.expErr {
