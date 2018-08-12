979b575e235d5ffe801829b886767f9f6a03c22a
Pablo RUTH <contact@pablo-ruth.fr>
2018-08-12 20:18:06 +0200

    Add a local signer backend (#11)
    * add a local signer backend
    
    * move to viper interface and add ssh key parsing
    
    * add tests to local signer
    
    * check error on random serial generation
    
    * refactoring CheckCertReqFields func

--- builtin/signer/Signer.go
+++ builtin/signer/Signer.go
@@ -1,6 +1,10 @@
 package signer
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 // Signer is the interface that wrap the SMK SSH Signing operation.
 type Signer interface {
@@ -14,4 +18,19 @@ type CertReq struct {
 	Key        string
 	ID         string
 	Principals []string
+}
+
+// CheckCertReqFields checks if all CertReq fields are set
+func (c CertReq) CheckCertReqFields() error {
+	if len(c.Principals) == 0 {
+		return fmt.Errorf("Empty list of principals")
+	}
+	if len(c.ID) == 0 {
+		return fmt.Errorf("Empty ID string")
+	}
+	if len(c.Key) == 0 {
+		return fmt.Errorf("Empty key string")
+	}
+
+	return nil
 }
---
+++ builtin/signer/local/Signer.go
@@ -0,0 +1,116 @@
+package local
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/signmykeyio/signmykey/builtin/signer"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/ssh"
+)
+
+// Signer struct represents Hashicorp Vault options for signing SSH Key.
+type Signer struct {
+	CACert          ssh.PublicKey
+	CAKey           ssh.Signer
+	TTL             int
+	CriticalOptions map[string]string
+	Extensions      map[string]string
+}
+
+// Init method is used to ingest config of Signer
+func (s *Signer) Init(config *viper.Viper) error {
+	neededEntries := []string{
+		"caCert",
+		"caKey",
+		"ttl",
+	}
+
+	for _, entry := range neededEntries {
+		if !config.IsSet(entry) {
+			return fmt.Errorf("Config entry %s missing for Signer", entry)
+		}
+	}
+
+	// Read and parse CA private key
+	key, err := ioutil.ReadFile(config.GetString("caKey"))
+	if err != nil {
+		return err
+	}
+	s.CAKey, err = ssh.ParsePrivateKey(key)
+	if err != nil {
+		return err
+	}
+
+	// Read and parse CA public key
+	pubKey, err := ioutil.ReadFile(config.GetString("caCert"))
+	if err != nil {
+		return err
+	}
+	s.CACert, err = ssh.ParsePublicKey(pubKey)
+	if err != nil {
+		return err
+	}
+
+	s.TTL = viper.GetInt("ttl")
+	s.CriticalOptions = config.GetStringMapString("criticalOptions")
+	s.Extensions = config.GetStringMapString("extensions")
+
+	return nil
+}
+
+// ReadCA method read CA public cert from local file
+func (s Signer) ReadCA() (string, error) {
+	return string(s.CACert.Marshal()), nil
+}
+
+// Sign method is used to sign passed SSH Key.
+func (s Signer) Sign(certreq signer.CertReq) (string, error) {
+
+	err := certreq.CheckCertReqFields()
+	if err != nil {
+		return "", errors.Wrap(err, "certificate request fields checking failed")
+	}
+
+	buf := make([]byte, 8)
+	_, err = rand.Read(buf)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to read random bytes")
+	}
+	serial := binary.LittleEndian.Uint64(buf)
+
+	pubKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(certreq.Key))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse user public key: %s", err)
+	}
+
+	certificate := ssh.Certificate{
+		Serial:          serial,
+		Key:             pubKey,
+		KeyId:           certreq.ID,
+		ValidPrincipals: certreq.Principals,
+		ValidAfter:      uint64(time.Now().Add(-1 * time.Minute).In(time.UTC).Unix()),
+		ValidBefore:     uint64(time.Now().Add(time.Duration(s.TTL) * time.Second).In(time.UTC).Unix()),
+		CertType:        ssh.UserCert,
+		Permissions: ssh.Permissions{
+			CriticalOptions: s.CriticalOptions,
+			Extensions:      s.Extensions,
+		},
+	}
+
+	err = certificate.SignCert(rand.Reader, s.CAKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign user public key: %s", err)
+	}
+
+	marshaledCertificate := ssh.MarshalAuthorizedKey(&certificate)
+	if len(marshaledCertificate) == 0 {
+		return "", errors.New("failed to marshal signed certificate, empty result")
+	}
+
+	return string(marshaledCertificate), nil
+}
