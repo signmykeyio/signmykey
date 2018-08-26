83c1b229cb3972a37bd21f8192808dad08a50c21
Pablo RUTH <contact@pablo-ruth.fr>
2018-08-26 16:19:52 +0200

    force TLS to 1.2 with secure ciphers

--- api/server.go
+++ api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
@@ -53,8 +54,26 @@ func Serve(startconfig Config) error {
 		return fmt.Errorf("Key file %s doesn't exist", err)
 	}
 
+	tlsCfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	srv := &http.Server{
+		Addr:         config.Addr,
+		Handler:      Router(),
+		TLSConfig:    tlsCfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
 	log.Printf("signmykey server listen on https://%s", config.Addr)
-	return http.ListenAndServeTLS(config.Addr, config.TLSCert, config.TLSKey, Router())
+	return srv.ListenAndServeTLS(config.TLSCert, config.TLSKey)
 }
 
 // Router returns *chi.Mux config
