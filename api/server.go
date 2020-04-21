fcafabb769632d3ff0e987e12e20930ab548b7f3
Pablo RUTH <contact@pablo-ruth.fr>
2020-04-21 15:23:26 +0200

    Fix TLS 1.2 ciphers and enable 1.3 config with ciphers (#69)

--- api/server.go
+++ api/server.go
@@ -58,8 +58,15 @@ func Serve(startconfig Config) error {
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
+			// TLS 1.3
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			// TLS 1.2, support will be remove from future version
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 		},
 	}
 	srv := &http.Server{
