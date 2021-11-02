5d2b989db9ba1963797b9792adeb361fab8b4375
Pablo RUTH <contact@pablo-ruth.fr>
2021-11-02 15:01:17 +0100

    replace deprecated github.com/howeyc/gopass by golang.org/x/term

--- cmd/hash.go
+++ cmd/hash.go
@@ -2,10 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/howeyc/gopass"
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/term"
 )
 
 var hashCmd = &cobra.Command{
@@ -14,7 +15,7 @@ var hashCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		fmt.Printf("Password to hash (will be hidden): ")
-		password, err := gopass.GetPasswd()
+		password, err := term.ReadPassword(int(os.Stdin.Fd()))
 		if err != nil {
 			return err
 		}
@@ -24,7 +25,7 @@ var hashCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Printf("Hashed password: %s\n", string(hash))
+		fmt.Printf("\nHashed password: %s\n", string(hash))
 
 		return nil
 	},
